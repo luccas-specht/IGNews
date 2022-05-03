f9e65f09cb7a1c1e7d3c71c20cfb09165a1bf4f8
Luccas Specht <luccasspecht70@gmail.com>
2022-05-03 08:26:39 -0300

    refact (stripe): update to set a static stripe key

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,11 @@ import Stripe from 'stripe';
 
 import pack from '../../package.json';
 
-export const stripe = new Stripe(String(process.env.STRIPE_API_KEY), {
+// TODO: add stripe secret key to .env
+const randomKey =
+  'sk_test_51KnWlOGkw9LK4xGQI65WGbfGLoaab6Nqtaqwo7RgqIoqow8OjxZ3sQmZZTA8R4YY5YEph8cVXwvCgfzlhLnXwSOS00gTjVTv8M';
+
+export const stripe = new Stripe(randomKey, {
   apiVersion: '2020-08-27',
   appInfo: {
     name: 'Ignews',
