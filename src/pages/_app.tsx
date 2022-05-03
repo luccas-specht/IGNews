e87736d75b2e8fd2b46dab516e09eb384816fa04
Luccas Specht <luccasspecht70@gmail.com>
2022-05-03 09:51:12 -0300

    feat: add config to next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 //ever time when the page is loaded or changed, the app will be rendered and ir will be passed the page props
 
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
 import { Header } from '../components';
 
 import '../styles/global.scss';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
+const MyApp = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => (
+  <SessionProvider session={session}>
     <Header />
     <Component {...pageProps} />
-  </>
+  </SessionProvider>
 );
 
 export default MyApp;
