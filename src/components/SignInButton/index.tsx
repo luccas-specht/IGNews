fdc989b63ae5dc3f80442621b538f92c64724861
Luccas Specht <luccasspecht70@gmail.com>
2022-05-03 09:50:37 -0300

    feat(SignInButton): integraction auth with next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,29 @@
 import { FiX } from 'react-icons/fi';
 import { FaGithub } from 'react-icons/fa';
 
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = false;
+  const { data, status } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return status === 'authenticated' ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Luccas Specht
+      {data?.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
